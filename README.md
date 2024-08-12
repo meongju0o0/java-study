56c84831307804654a51c27287240065e50fa436
meongju0o0 <bl4angel20@khu.ac.kr>
2024-08-12 19:00:33 +0900

    .

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Java의 정석 3rd Edition
 ## Chap 01
+- Java의 역사와 장단점
 - Install JDK
 - Hello World
+- 출력 함수
 
 ## Chap 02
 - 변수
 - 자료형
+- 입력 함수
+- 묵시적 형변환
+- 명시적 형변환
 
 ## Chap 03
 - 연산자
---
+++ src/Chap03/OperatorEx10.java
@@ -0,0 +1,12 @@
+package Chap03;
+
+public class OperatorEx10 {
+    public static void main(String[] args) {
+        int a = 1000000;
+
+        int result1 = a * a / a; // overflow occurs
+        int result2 = a / a * a;
+        System.out.printf("%d * %d / %d = %d\n", a, a, a, result1);
+        System.out.printf("%d / %d * %d = %d\n", a, a, a, result2);
+    }
+}
